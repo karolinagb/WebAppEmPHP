7c9732036865d2258a387bc020333c85a71ec08c
x <karolina_bp@hotmail.com>
2022-07-11 21:04:47 -0300

    Edição de um artigo

--- phpweb-master/admin/index.php
+++ phpweb-master/admin/index.php
@@ -28,7 +28,7 @@ $artigos = $artigo->exibeTodos();
             <div id="artigo-admin">
                 <p> <?php echo $artigo['titulo']; ?> </p>
                 <nav>
-                    <a class="botao" href="editar-artigo.html?id=<?php echo $artigo['id'];?>">Editar</a>
+                    <a class="botao" href="editar-artigo.php?id=<?php echo $artigo['id'];?>">Editar</a>
                     <a class="botao" href="excluir-artigo.php?id=<?php echo $artigo['id'];?>">Excluir</a>
                 </nav>
             </div>
--- phpweb-master/src/Artigo.php
+++ phpweb-master/src/Artigo.php
@@ -56,4 +56,13 @@ class Artigo
 
         $removerArtigo->execute();
     }
+
+    public function editar(string $id, string $titulo, string $conteudo): void
+    {
+        $editarArtigo = $this->mysql->prepare('UPDATE artigos SET titulo = ?, conteudo = ? WHERE id = ?');
+
+        $editarArtigo->bind_param('sss', $titulo, $conteudo, $id);
+
+        $editarArtigo->execute();
+    }
 }
